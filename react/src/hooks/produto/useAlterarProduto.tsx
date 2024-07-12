7d013b16e9efd7b99e904147f2dd6dc17a39a5d1
ssadoraa <isadoraverbicario@hotmail.com>
2024-07-12 18:08:15 -0300

    ajuste

--- react/src/hooks/produto/useAlterarProduto.tsx
+++ react/src/hooks/produto/useAlterarProduto.tsx
@@ -1,19 +1,25 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useNavigate } from "react-router-dom";
 import Produto from "../../interfaces/produto";
 import { URL_PRODUTO } from "../../util/constants";
 import useAPI from "../useAPI";
 
 const useAlterarProduto = () => {
   const { alterar } = useAPI<Produto>(URL_PRODUTO);
-
   const queryClient = useQueryClient();
+  const navigate = useNavigate();
 
-  return useMutation({
+  const mutation = useMutation({
     mutationFn: (produto: Produto) => alterar(produto),
-    onSuccess: () =>
+    onSuccess: (data: Produto) => {
       queryClient.invalidateQueries({
         queryKey: ["produtos"],
-      }),
+      });
+      navigate(`/${data.id}`);
+    },
   });
+
+  return mutation;
 };
+
 export default useAlterarProduto;
--- react/src/hooks/produto/useCadastrarProduto.tsx
+++ react/src/hooks/produto/useCadastrarProduto.tsx
@@ -1,19 +1,25 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useNavigate } from "react-router-dom";
 import Produto from "../../interfaces/produto";
 import { URL_PRODUTO } from "../../util/constants";
 import useAPI from "../useAPI";
 
 const useCadastrarProduto = () => {
   const { cadastrar } = useAPI<Produto>(URL_PRODUTO);
-
   const queryClient = useQueryClient();
+  const navigate = useNavigate();
 
-  return useMutation({
+  const mutation = useMutation({
     mutationFn: (produto: Produto) => cadastrar(produto),
-    onSuccess: () =>
+    onSuccess: () => {
       queryClient.invalidateQueries({
         queryKey: ["produtos"],
-      }),
+      });
+      navigate("/meus_produtos");
+    },
   });
+
+  return mutation;
 };
+
 export default useCadastrarProduto;
