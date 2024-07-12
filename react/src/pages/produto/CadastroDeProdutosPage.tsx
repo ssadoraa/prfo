9afddf585029149a537e8884b8a492599ddbabc9
ssadoraa <isadoraverbicario@hotmail.com>
2024-07-12 14:17:29 -0300

    ajuste

--- react/src/pages/produto/CadastroDeProdutosPage.tsx
+++ react/src/pages/produto/CadastroDeProdutosPage.tsx
@@ -5,29 +5,30 @@ import CadastroDeProdutosForm from "../../components/CadastroDeProdutosForm";
 import "../../css/geral.css";
 import useAPI from "../../hooks/useAPI";
 import Produto from "../../interfaces/produto";
+import { URL_PRODUTO } from "../../util/constants";
 
 const CadastroDeProdutosPage = () => {
   const { id } = useParams();
-  const { setProdutoSelecionado } = useProdutoStore();
-  const { recuperarById } = useAPI<Produto>("produtos");
+  const setProdutoSelecionado = useProdutoStore((s) => s.setProdutoSelecionado);
+  const { recuperarById } = useAPI<Produto>(URL_PRODUTO);
 
   useEffect(() => {
-    const loadProduto = async () => {
-      if (id) {
-        try {
-          const produto: Produto = await recuperarById(Number(id));
+    if (id) {
+      recuperarById(Number(id))
+        .then(produto => {
           setProdutoSelecionado(produto);
-        } catch (error) {
-          console.error("Erro ao carregar produto:", error);
-        }
-      }
-    };
-    loadProduto();
-  }, [id, recuperarById, setProdutoSelecionado]);
+        })
+        .catch(error => {
+          console.error("Erro ao obter o produto:", error);
+        });
+    } else {
+      setProdutoSelecionado({} as Produto);
+    }
+  }, [id, setProdutoSelecionado, recuperarById]);
 
   return (
     <>
-      <h4 className="mt-4">Cadastro de Produtos</h4>
+      <h4 className="mt-4">{id ? "Alterar Produto" : "Cadastrar Produto"}</h4>
       <hr className="mt-1 mb-4" />
       <CadastroDeProdutosForm />
     </>
